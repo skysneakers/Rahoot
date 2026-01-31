8528cef2d31916705b14e28df0bfb09036bdcb08
skysneakers <gwen.lincroft@gmail.com>
2026-01-31 15:36:40 -0500

    Testing EC2

--- packages/web/src/app/(auth)/layout.tsx
+++ packages/web/src/app/(auth)/layout.tsx
@@ -17,7 +17,7 @@ const AuthLayout = ({ children }: PropsWithChildren) => {
   if (!isConnected) {
     return (
       <section className="relative flex min-h-dvh flex-col items-center justify-center">
-        <div className="absolute h-full w-full overflow-hidden">
+        <div className="pointer-events-none absolute h-full w-full overflow-hidden">
           <div className="bg-primary/15 absolute -top-[15vmin] -left-[15vmin] min-h-[75vmin] min-w-[75vmin] rounded-full"></div>
           <div className="bg-primary/15 absolute -right-[15vmin] -bottom-[15vmin] min-h-[75vmin] min-w-[75vmin] rotate-45"></div>
         </div>
@@ -33,7 +33,7 @@ const AuthLayout = ({ children }: PropsWithChildren) => {
 
   return (
     <section className="relative flex min-h-dvh flex-col items-center justify-center">
-      <div className="absolute h-full w-full overflow-hidden">
+      <div className="pointer-events-none absolute h-full w-full overflow-hidden">
         <div className="bg-primary/15 absolute -top-[15vmin] -left-[15vmin] min-h-[75vmin] min-w-[75vmin] rounded-full"></div>
         <div className="bg-primary/15 absolute -right-[15vmin] -bottom-[15vmin] min-h-[75vmin] min-w-[75vmin] rotate-45"></div>
       </div>
